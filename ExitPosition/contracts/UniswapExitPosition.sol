6754e3ef53cb25e02b7b529a97dca1ec5d27714f
yaheda <yaheda.a@gmail.com>
2022-12-15 20:16:53 +0200

    create exit position in contract

--- ExitPosition/contracts/UniswapExitPosition.sol
+++ ExitPosition/contracts/UniswapExitPosition.sol
@@ -1,23 +1,67 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity = 0.7.6;
+pragma abicoder v2;
 
 import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
 
-contract UniswapExitPosition {
+contract UniswapExitPosition is IERC721Receiver {
 
-  function exitPosition(uint256 tokenId) external returns (uint256 amount0, uint256 amount1) {
+    address public nonfungiblePositionAddress = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
+    INonfungiblePositionManager public immutable nonfungiblePositionManager;
+
+    struct Deposit {
+        address owner;
+        uint128 liquidity;
+        address token0;
+        address token1;
+    }
+
+    mapping(uint256 => Deposit) public deposits;
+
+    constructor(
+        //INonfungiblePositionManager _nonfungiblePositionManager
+    ) {
+        //nonfungiblePositionManager = _nonfungiblePositionManager;
+        nonfungiblePositionManager = INonfungiblePositionManager(nonfungiblePositionAddress);
+    }
+
+    function onERC721Received(
+        address operator,
+        address,
+        uint256 tokenId,
+        bytes calldata
+    ) external override returns (bytes4) {
+        // get position information
+
+        _createDeposit(operator, tokenId);
+
+        return this.onERC721Received.selector;
+    }
+
+    function _createDeposit(address owner, uint256 tokenId) internal {
+        (, , address token0, address token1, , , , uint128 liquidity, , , , ) =
+            nonfungiblePositionManager.positions(tokenId);
+
+        // set the owner and data for position
+        // operator is msg.sender
+        deposits[tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
+    }
+
+    function exitPosition(uint256 tokenId) external returns (uint256 amount0, uint256 amount1) {
         // caller must be the owner of the NFT
         require(msg.sender == deposits[tokenId].owner, 'Not the owner');
         // get liquidity data for tokenId
-        uint128 liquidity = deposits[tokenId].liquidity;
-        uint128 halfLiquidity = liquidity / 2;
+        //uint128 liquidity = deposits[tokenId].liquidity;
+        uint128 exitLiquidity = 0; //liquidity / 2;
 
         // amount0Min and amount1Min are price slippage checks
         // if the amount received after burning is not greater than these minimums, transaction will fail
         INonfungiblePositionManager.DecreaseLiquidityParams memory params =
             INonfungiblePositionManager.DecreaseLiquidityParams({
                 tokenId: tokenId,
-                liquidity: halfLiquidity,
+                liquidity: exitLiquidity,
                 amount0Min: 0,
                 amount1Min: 0,
                 deadline: block.timestamp
@@ -27,6 +71,21 @@ contract UniswapExitPosition {
 
         //send liquidity back to owner
         _sendToOwner(tokenId, amount0, amount1);
+    }
+
+    function _sendToOwner(
+        uint256 tokenId,
+        uint256 amount0,
+        uint256 amount1
+    ) internal {
+        // get owner of contract
+        address owner = deposits[tokenId].owner;
+
+        address token0 = deposits[tokenId].token0;
+        address token1 = deposits[tokenId].token1;
+        // send collected fees to owner
+        TransferHelper.safeTransfer(token0, owner, amount0);
+        TransferHelper.safeTransfer(token1, owner, amount1);
     }
 
 }
