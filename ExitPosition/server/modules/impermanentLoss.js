167fd3306af5b4856440333d4666839f8fddefab
yaheda <yaheda.a@gmail.com>
2022-12-16 12:05:15 +0200

    getPoolInfo

--- ExitPosition/server/modules/impermanentLoss.js
+++ ExitPosition/server/modules/impermanentLoss.js
@@ -26,9 +26,11 @@ function buildPositionQuery (positionId) {
 }
 
 function buildPoolQuery (poolId) {
-  return `pools(where: {id: ${poolId}}) {
-    tick
-    sqrtPrice
+  return `{
+    pools(where: {id: "${poolId}"}) {
+      tick
+      sqrtPrice
+    }
   }`;
 }
 
@@ -42,16 +44,49 @@ async function getPositionInfo(url, positionId) {
     { query: buildPositionQuery(positionId) }, 
     { headers: { "Accept-Encoding": "gzip,deflate,compress" } 
   });
-  return response.data["pools"][0];
+
+  if (response.data.data["positions"] == 0)
+    throw new Error('position not found');
+
+  var position = response.data.data["positions"][0];
+
+  return {
+    liquidity: parseInt(position["liquidity"]),
+    tick_lower: parseInt(position["tickLower"]["tickIdx"]),
+    tick_upper: parseInt(position["tickUpper"]["tickIdx"]),
+    pool_id: position["pool"]["id"],
+
+    token0: position["token0"]["symbol"],
+    token1: position["token1"]["symbol"],
+    decimals0: parseInt(position["token0"]["decimals"]),
+    decimals1: parseInt(position["token1"]["decimals"]),
+  }
+
+  
 }
 
 async function getPoolInfo(url, poolId) {
+  const response = await axios.post(
+    url,     
+    { query: buildPoolQuery(poolId) }, 
+    { headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+  });
 
+  if (response.data.data["pools"] == 0)
+    throw new Error('pool not found');
+  
+  var pool = response.data.data["pools"][0]
+  
+  return {
+    current_tick: parseInt(pool["tick"]),
+    current_sqrt_price: parseInt(pool["sqrtPrice"]) / (2 ** 96)
+  };
 }
 
 async function getLoss(url, positionId) {
   
   var positionInfo = await getPositionInfo(url, positionId);
+  var poolPositionInfo = await getPoolInfo(url, positionInfo.pool.id)
   
   debugger;
   return {
